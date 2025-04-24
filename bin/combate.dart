f6797e36ff5dcf0a70c43b47ddad7e800e45c1ef
Aaron <@pepechiqui>
2025-04-24 18:48:31 +0200

    Pelea hecha

--- bin/combate.dart
+++ bin/combate.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'dart:math';
+
 class Combate {
   int? _vidaUsuario;
   int? _vidaMonstruo;
@@ -20,5 +23,45 @@ class Combate {
   }
   set ataqueMonstruo(int? ataqueM) {
     _ataqueMonstruo = ataqueM;
+  }
+
+  pelea() {
+    bool golpear = pegar();
+      do {
+        pegar();
+      if (golpear){
+        vidaMonstruo = vidaMonstruo! - ataqueUsuario!;
+      }else{
+        vidaUsuario = vidaUsuario! - ataqueMonstruo!;
+      }
+    } while (vidaUsuario! > 0 && vidaMonstruo! > 0);
+  }
+
+  bool pegar(){
+    bool golpe = false;
+    do{
+      int numeroSecreto=Random().nextInt(99)+1;
+      stdout.writeln ("adivina el numero secreto");
+      int intentos=0;
+      int intMAX=10;
+      while(intentos<intMAX){
+        String respuesta= stdin.readLineSync() ?? "error";
+        int? numero = int.tryParse(respuesta);
+        stdout.writeln("intentos=$intentos");
+        intentos++;
+        if(numero==null){
+          stdout.writeln("eso no es un numero");
+        }else if(numero>numeroSecreto){
+          stdout.writeln("el numero es mas bajo");
+        }else if(numero<numeroSecreto){
+          stdout.writeln("el numero es mas alto");
+        }else if(numero==numeroSecreto){
+          stdout.writeln("acertaste, golpeas");
+          golpe=true;
+          break;
+        }
+      }
+    }while(golpe==false);
+    return golpe;
   }
 }
